cb7fb4d198ce94a42efb4c8fc879509c00d91e4a
Dileep Kishore <k.dileep1994@gmail.com>
2022-04-18 17:08:04 +0530

    Add: dFBA env and hyperparams to RLZoo

--- utils/import_envs.py
+++ utils/import_envs.py
@@ -32,3 +32,46 @@ try:
     import panda_gym  # pytype: disable=import-error
 except ImportError:
     panda_gym = None
+
+try:
+    from relearn.environments import dFBAEnv
+    from gym.envs.registration import register
+
+    param_dict = {
+        "C0,in": 20,
+        "q": 0.5,
+        "vmax": [10.0, 5.0],
+        "vmax0": [5.0, 5.0],
+        "Km0": [1e-4, 1e-4],
+        "Km": [2e-3, 1e-3],
+        "models": [
+            "../../ReLearn/data/covert/mutant1.json",
+            "../../ReLearn/data/covert/mutant2.json",
+        ]
+    }
+    env_dict = {
+      "initial_N": [
+        [2, 3],
+        [3, 3],
+        [2, 2],
+        [3, 2]
+      ],
+      "target_N": [
+        [2, 3],
+        [3, 3],
+        [2, 2],
+        [3, 2]
+      ],
+      "initial_C": [0.0, 0.0],
+      "initial_C0": 10.0,
+      "Cin_bounds": [0.0, 20],
+      "sampling_rate": 0.0833
+    }
+    register(
+        id="dFBA-v1",
+        entry_point=dFBAEnv,
+        max_episode_steps=200,
+        kwargs={"param_dict": param_dict, "env_dict": env_dict, "dt": 1/720, "ep_length": 200}
+    )
+except ImportError:
+    dFBAEnv = None
